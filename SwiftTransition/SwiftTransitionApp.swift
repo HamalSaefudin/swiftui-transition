8edb655ecc5801a339b36dc094631c81cda56057
Hamal <Hamalsaefudin48@gmail.com>
2022-12-10 09:19:11 +0700

    Initial Commit

---
+++ SwiftTransition/SwiftTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTransitionApp.swift
+//  SwiftTransition
+//
+//  Created by Hamal Saefudin on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
